a892a09c9acf3938934c8d2c7587a7b82da4c794
SomeoneWithAnInternetConnection <34518321+SomeoneWithAnInternetConnection@users.noreply.github.com>
2018-02-20 15:56:52 -0500

    Improve Grand Exchange offer state detection

--- runelite-api/src/main/java/net/runelite/api/GrandExchangeOfferState.java
+++ runelite-api/src/main/java/net/runelite/api/GrandExchangeOfferState.java
@@ -1,7 +1,5 @@
 package net.runelite.api;
 
-import java.util.Arrays;
-
 /**
  * Describes the state of a Grand Exchange offer
  */
@@ -10,51 +8,25 @@ public enum GrandExchangeOfferState
 	/**
 	 * An empty slot.
 	 */
-	EMPTY(-1),
+	EMPTY,
+	/**
+	 * Any offer that's been cancelled
+	 */
+	CANCELLED,
 	/**
 	 * A buy offer that is currently in progress.
 	 */
-	BUYING(2),
+	BUYING,
 	/**
 	 * A buy offer that has completed.
 	 */
-	BOUGHT(5),
+	BOUGHT,
 	/**
 	 * A sell offer that is currently in progress.
 	 */
-	SELLING(10),
+	SELLING,
 	/**
 	 * A sell offer that has completed.
 	 */
-	SOLD(13);
-
-	private final int code;
-
-	GrandExchangeOfferState(int code)
-	{
-		this.code = code;
-	}
-
-	/**
-	 * The offer code given by #getProgress();
-	 * @return
-	 */
-	private int getCode()
-	{
-		return code;
-	}
-
-	/**
-	 * Returns the OfferState instance associated with the given code.
-	 *
-	 * @param code The code to search for
-	 * @return The respective OfferState instance
-	 */
-	public static GrandExchangeOfferState progressToState(byte code)
-	{
-		return Arrays.stream(values())
-			.filter(offerState -> offerState.getCode() == code)
-			.findFirst()
-			.orElse(EMPTY);
-	}
+	SOLD;
 }
--- runelite-mixins/src/main/java/net/runelite/mixins/RSGrandExchangeOfferMixin.java
+++ runelite-mixins/src/main/java/net/runelite/mixins/RSGrandExchangeOfferMixin.java
@@ -1,6 +1,12 @@
 package net.runelite.mixins;
 
 import net.runelite.api.GrandExchangeOfferState;
+import static net.runelite.api.GrandExchangeOfferState.BOUGHT;
+import static net.runelite.api.GrandExchangeOfferState.BUYING;
+import static net.runelite.api.GrandExchangeOfferState.CANCELLED;
+import static net.runelite.api.GrandExchangeOfferState.EMPTY;
+import static net.runelite.api.GrandExchangeOfferState.SELLING;
+import static net.runelite.api.GrandExchangeOfferState.SOLD;
 import net.runelite.api.mixins.Inject;
 import net.runelite.api.mixins.Mixin;
 import net.runelite.rs.api.RSGrandExchangeOffer;
@@ -8,10 +14,70 @@ import net.runelite.rs.api.RSGrandExchangeOffer;
 @Mixin(RSGrandExchangeOffer.class)
 public abstract class RSGrandExchangeOfferMixin implements RSGrandExchangeOffer
 {
+
+
 	@Inject
 	@Override
 	public GrandExchangeOfferState getState()
 	{
-		return GrandExchangeOfferState.progressToState(getRSState());
+		/*
+		 Internally a GrandExchangeOffer's state is represented as 4 flags
+		 packed into the lower half of a byte. They are:
+		*/
+
+		//Set for sell offers, unset for buy offers
+		int IS_SELLING = 1 << 3; // 0b1000
+
+
+		/*
+		Set for offers that have finished, either because they've
+		been filled, or because they were cancelled
+		*/
+		int COMPLETED = 1 << 2; // 0b0100
+
+		/*
+		Set for offers that are actually live
+		NB: Insta-buy/sell offers will be simultaneously LIVE and LOCAL
+		*/
+		int LIVE = 1 << 1; // 0b0010
+
+		//True for just-made, just-cancelled, completely cancelled, and completed offers
+		int LOCAL = 1;
+
+		byte code = getRSState();
+		boolean isSelling = (code & IS_SELLING) == IS_SELLING;
+		boolean isFinished = (code & COMPLETED) == COMPLETED;
+
+
+		if (code == 0)
+		{
+			return EMPTY;
+		}
+		else if (getQuantitySold() < getTotalQuantity())
+		{
+			return CANCELLED;
+		}
+		else if (isSelling)
+		{
+			if (isFinished)
+			{
+				return SOLD;
+			}
+			else // if isUnfinished
+			{
+				return SELLING;
+			}
+		}
+		else // if isBuying
+		{
+			if (isFinished)
+			{
+				return BOUGHT;
+			}
+			else // if isUnfinished
+			{
+				return BUYING;
+			}
+		}
 	}
 }
