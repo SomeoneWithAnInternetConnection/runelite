71ac8e4698a6ab31428f0da34b0c3204516dc233
SomeoneWithAnInternetConnection <34518321+SomeoneWithAnInternetConnection@users.noreply.github.com>
2018-03-12 21:20:01 -0400

    Checkstyle ItemSpriteFactory

--- cache/src/main/java/net/runelite/cache/item/ItemSpriteFactory.java
+++ cache/src/main/java/net/runelite/cache/item/ItemSpriteFactory.java
@@ -15,11 +15,14 @@ public class ItemSpriteFactory
 {
 
 
-	public static final SpritePixels createSprite(Store store , ItemDefinition var9, int quantity, int border, int shadowColor, int stackable, boolean noted) throws IOException
+	public static final SpritePixels createSprite(Store store, ItemDefinition var9, int quantity, int border, int shadowColor, int stackable, boolean noted) throws IOException
 	{
-		if(quantity == -1) {
+		if (quantity == -1)
+		{
 			stackable = 0;
-		} else if(stackable == 2 && quantity != 1) {
+		}
+		else if (stackable == 2 && quantity != 1)
+		{
 			stackable = 1;
 		}
 
@@ -34,11 +37,14 @@ public class ItemSpriteFactory
 
 //		ItemDefinition var9 = null;
 //		ItemComposition var9 = class81.getItemDefinition(itemId);
-		if(quantity > 1 && var9.countObj != null) {
+		if (quantity > 1 && var9.countObj != null)
+		{
 			int var10 = -1;
 
-			for(int var11 = 0; var11 < 10; ++var11) {
-				if(quantity >= var9.countCo[var11] && var9.countCo[var11] != 0) {
+			for (int var11 = 0; var11 < 10; ++var11)
+			{
+				if (quantity >= var9.countCo[var11] && var9.countCo[var11] != 0)
+				{
 					var10 = var9.countObj[var11];
 				}
 			}
@@ -51,24 +57,27 @@ public class ItemSpriteFactory
 //		ModelDefinition var19 = null;
 		Index models = store.getIndex(IndexType.MODELS);
 //		models.getArchive()
-		Model var19 = getModel (store, var9,1) ; //var9.getModel(1);
-		if(var19 == null) {
+		Model var19 = getModel(store, var9, 1); //var9.getModel(1);
+		if (var19 == null)
+		{
 			return null;
-		} else {
-			SpritePixels var20 = null;
+		}
+		else
+		{
+			SpritePixels spritePixels = null;
 //			if(var9.notedTemplate != -1) {
-//				var20 = createSprite(var9.notedID, 10, 1, 0, 0, true);
-//				if(var20 == null) {
+//				spritePixels = createSprite(var9.notedID, 10, 1, 0, 0, true);
+//				if(spritePixels == null) {
 //					return null;
 //				}
 //			} else if(var9.boughtTemplateId != -1) {
-//				var20 = createSprite(var9.boughtId, quantity, border, shadowColor, 0, false);
-//				if(var20 == null) {
+//				spritePixels = createSprite(var9.boughtId, quantity, border, shadowColor, 0, false);
+//				if(spritePixels == null) {
 //					return null;
 //				}
 //			} else if(var9.placeholderTemplateId != -1) {
-//				var20 = createSprite(var9.placeholderId, quantity, 0, 0, 0, false);
-//				if(var20 == null) {
+//				spritePixels = createSprite(var9.placeholderId, quantity, 0, 0, 0, false);
+//				if(spritePixels == null) {
 //					return null;
 //				}
 //			}
@@ -84,40 +93,49 @@ public class ItemSpriteFactory
 			Graphics3D.Rasterizer3D_method1();
 			Graphics3D.method2780(16, 16);
 			Graphics3D.rasterGouraudLowRes = false;
-			if(var9.placeholderTemplateId != -1) {
-				var20.drawAt(0, 0);
+			if (var9.placeholderTemplateId != -1)
+			{
+				spritePixels.drawAt(0, 0);
 			}
 
 			int var16 = var9.zoom2d;
-			if(noted) {
-				var16 = (int)((double)var16 * 1.5D);
-			} else if(border == 2) {
-				var16 = (int)((double)var16 * 1.04D);
+			if (noted)
+			{
+				var16 = (int) ((double) var16 * 1.5D);
+			}
+			else if (border == 2)
+			{
+				var16 = (int) ((double) var16 * 1.04D);
 			}
 
 			int var17 = var16 * Graphics3D.SINE[var9.xan2d] >> 16;
 			int var18 = var16 * Graphics3D.COSINE[var9.xan2d] >> 16;
 			var19.calculateBoundsCylinder();
 			var19.method2702(0, var9.yan2d, var9.zan2d, var9.xan2d, var9.xOffset2d, var19.modelHeight / 2 + var17 + var9.yOffset2d, var18 + var9.yOffset2d);
-			if(var9.boughtTemplateId != -1) {
-				var20.drawAt(0, 0);
+			if (var9.boughtTemplateId != -1)
+			{
+				spritePixels.drawAt(0, 0);
 			}
 
-			if(border >= 1) {
+			if (border >= 1)
+			{
 				var8.method5838(1);
 			}
 
-			if(border >= 2) {
+			if (border >= 2)
+			{
 				var8.method5838(16777215);
 			}
 
-			if(shadowColor != 0) {
+			if (shadowColor != 0)
+			{
 				var8.method5886(shadowColor);
 			}
 
 			Rasterizer2D.setRasterBuffer(var8.pixels, 36, 32);
-			if(var9.notedTemplate != -1) {
-				var20.drawAt(0, 0);
+			if (var9.notedTemplate != -1)
+			{
+				spritePixels.drawAt(0, 0);
 			}
 
 //			if(stackable == 1 || stackable == 2 && var9.stackable == 1) {
@@ -164,19 +182,25 @@ public class ItemSpriteFactory
 		Model var5;
 		ModelDefinition var6 = new ModelLoader().load(item.inventoryModel, data);
 //			ModelData var6 = ModelData.method2594(ItemDefinition_modelIndexCache, item.inventoryModel, 0);
-			if(var6 == null) {
-				return null;
-			} else {
-				if(item.resizeX != 128 || item.resizeY != 128 || item.resizeZ != 128) {
-					var6.method2610(item.resizeX, item.resizeY, item.resizeZ);
-				}
+		if (var6 == null)
+		{
+			return null;
+		}
+		else
+		{
+			if (item.resizeX != 128 || item.resizeY != 128 || item.resizeZ != 128)
+			{
+				var6.method2610(item.resizeX, item.resizeY, item.resizeZ);
+			}
 
-				int var4;
-				if(item.colorFind != null) {
-					for(var4 = 0; var4 < item.colorFind.length; ++var4) {
-						var6.recolor(item.colorFind[var4], item.colorReplace[var4]);
-					}
+			int var4;
+			if (item.colorFind != null)
+			{
+				for (var4 = 0; var4 < item.colorFind.length; ++var4)
+				{
+					var6.recolor(item.colorFind[var4], item.colorReplace[var4]);
 				}
+			}
 
 //				if(item.textureFind != null) {
 //					for(var4 = 0; var4 < item.textureFind.length; ++var4) {
@@ -184,36 +208,42 @@ public class ItemSpriteFactory
 //					}
 //				}
 
-				var5 = light(var6,item.ambient + 64, item.contrast + 768, -50, -10, -50);
-				var5.field1849 = true;
-				//itemModelCache.put(var5, (long)this.id);
-				return var5;
-			}
+			var5 = light(var6, item.ambient + 64, item.contrast + 768, -50, -10, -50);
+			var5.field1849 = true;
+			//itemModelCache.put(var5, (long)this.id);
+			return var5;
+		}
 		//}
 	}
 
-	public static Model light(ModelDefinition def, int var1, int var2, int var3, int var4, int var5) {
+	public static Model light(ModelDefinition def, int var1, int var2, int var3, int var4, int var5)
+	{
 		def.computeNormals();
-		int var6 = (int)Math.sqrt((double)(var5 * var5 + var3 * var3 + var4 * var4));
+		int var6 = (int) Math.sqrt((double) (var5 * var5 + var3 * var3 + var4 * var4));
 		int var7 = var6 * var2 >> 8;
 		Model var8 = new Model();
 		var8.field1856 = new int[def.faceCount];
 		var8.field1854 = new int[def.faceCount];
 		var8.field1823 = new int[def.faceCount];
-		if(def.textureTriangleCount > 0 && def.textureCoordinates != null) {
+		if (def.textureTriangleCount > 0 && def.textureCoordinates != null)
+		{
 			int[] var9 = new int[def.textureTriangleCount];
 
 			int var10;
-			for(var10 = 0; var10 < def.faceCount; ++var10) {
-				if(def.textureCoordinates[var10] != -1) {
+			for (var10 = 0; var10 < def.faceCount; ++var10)
+			{
+				if (def.textureCoordinates[var10] != -1)
+				{
 					++var9[def.textureCoordinates[var10] & 255];
 				}
 			}
 
 			var8.field1852 = 0;
 
-			for(var10 = 0; var10 < def.textureTriangleCount; ++var10) {
-				if(var9[var10] > 0 && def.textureRenderTypes[var10] == 0) {
+			for (var10 = 0; var10 < def.textureTriangleCount; ++var10)
+			{
+				if (var9[var10] > 0 && def.textureRenderTypes[var10] == 0)
+				{
 					++var8.field1852;
 				}
 			}
@@ -224,80 +254,109 @@ public class ItemSpriteFactory
 			var10 = 0;
 
 			int var11;
-			for(var11 = 0; var11 < def.textureTriangleCount; ++var11) {
-				if(var9[var11] > 0 && def.textureRenderTypes[var11] == 0) {
+			for (var11 = 0; var11 < def.textureTriangleCount; ++var11)
+			{
+				if (var9[var11] > 0 && def.textureRenderTypes[var11] == 0)
+				{
 					var8.field1844[var10] = def.textureTriangleVertexIndices1[var11] & '\uffff';
 					var8.field1865[var10] = def.textureTriangleVertexIndices2[var11] & '\uffff';
 					var8.field1846[var10] = def.textureTriangleVertexIndices3[var11] & '\uffff';
 					var9[var11] = var10++;
-				} else {
+				}
+				else
+				{
 					var9[var11] = -1;
 				}
 			}
 
 			var8.field1840 = new byte[def.faceCount];
 
-			for(var11 = 0; var11 < def.faceCount; ++var11) {
-				if(def.textureCoordinates[var11] != -1) {
-					var8.field1840[var11] = (byte)var9[def.textureCoordinates[var11] & 255];
-				} else {
+			for (var11 = 0; var11 < def.faceCount; ++var11)
+			{
+				if (def.textureCoordinates[var11] != -1)
+				{
+					var8.field1840[var11] = (byte) var9[def.textureCoordinates[var11] & 255];
+				}
+				else
+				{
 					var8.field1840[var11] = -1;
 				}
 			}
 		}
 
-		for(int var16 = 0; var16 < def.faceCount; ++var16) {
+		for (int var16 = 0; var16 < def.faceCount; ++var16)
+		{
 			byte var17;
-			if(def.faceRenderTypes == null) {
+			if (def.faceRenderTypes == null)
+			{
 				var17 = 0;
-			} else {
+			}
+			else
+			{
 				var17 = def.faceRenderTypes[var16];
 			}
 
 			byte var18;
-			if(def.faceAlphas == null) {
+			if (def.faceAlphas == null)
+			{
 				var18 = 0;
-			} else {
+			}
+			else
+			{
 				var18 = def.faceAlphas[var16];
 			}
 
 			short var12;
-			if(def.faceTextures == null) {
+			if (def.faceTextures == null)
+			{
 				var12 = -1;
-			} else {
+			}
+			else
+			{
 				var12 = def.faceTextures[var16];
 			}
 
-			if(var18 == -2) {
+			if (var18 == -2)
+			{
 				var17 = 3;
 			}
 
-			if(var18 == -1) {
+			if (var18 == -1)
+			{
 				var17 = 2;
 			}
 
 			VertexNormal var13;
 			int var14;
 			FaceNormal var19;
-			if(var12 == -1) {
-				if(var17 != 0) {
-					if(var17 == 1) {
+			if (var12 == -1)
+			{
+				if (var17 != 0)
+				{
+					if (var17 == 1)
+					{
 						var19 = def.faceNormals[var16];
 						var14 = (var4 * var19.y + var5 * var19.z + var3 * var19.x) / (var7 / 2 + var7) + var1;
 						var8.field1856[var16] = method2608(def.faceColors[var16] & '\uffff', var14);
 						var8.field1823[var16] = -1;
-					} else if(var17 == 3) {
+					}
+					else if (var17 == 3)
+					{
 						var8.field1856[var16] = 128;
 						var8.field1823[var16] = -1;
-					} else {
+					}
+					else
+					{
 						var8.field1823[var16] = -2;
 					}
-				} else {
+				}
+				else
+				{
 					int var15 = def.faceColors[var16] & '\uffff';
 //					if(def.field1741 != null && def.field1741[def.faceVertexIndices1[var16]] != null) {
 //						var13 = def.field1741[def.faceVertexIndices1[var16]];
 //					} else {
-						var13 = def.vertexNormals[def.faceVertexIndices1[var16]];
+					var13 = def.vertexNormals[def.faceVertexIndices1[var16]];
 					//}
 
 					var14 = (var4 * var13.y + var5 * var13.z + var3 * var13.x) / (var7 * var13.magnitude) + var1;
@@ -305,34 +364,41 @@ public class ItemSpriteFactory
 //					if(def.field1741 != null && def.field1741[def.faceVertexIndices2[var16]] != null) {
 //						var13 = def.field1741[def.faceVertexIndices2[var16]];
 //					} else {
-						var13 = def.vertexNormals[def.faceVertexIndices2[var16]];
-				//	}
+					var13 = def.vertexNormals[def.faceVertexIndices2[var16]];
+					//	}
 
 					var14 = (var4 * var13.y + var5 * var13.z + var3 * var13.x) / (var7 * var13.magnitude) + var1;
 					var8.field1854[var16] = method2608(var15, var14);
 //					if(def.field1741 != null && def.field1741[def.faceVertexIndices3[var16]] != null) {
 //						var13 = def.field1741[def.faceVertexIndices3[var16]];
 //					} else {
-						var13 = def.vertexNormals[def.faceVertexIndices3[var16]];
+					var13 = def.vertexNormals[def.faceVertexIndices3[var16]];
 					//}
 
 					var14 = (var4 * var13.y + var5 * var13.z + var3 * var13.x) / (var7 * var13.magnitude) + var1;
 					var8.field1823[var16] = method2608(var15, var14);
 				}
-			} else if(var17 != 0) {
-				if(var17 == 1) {
+			}
+			else if (var17 != 0)
+			{
+				if (var17 == 1)
+				{
 					var19 = def.faceNormals[var16];
 					var14 = (var4 * var19.y + var5 * var19.z + var3 * var19.x) / (var7 / 2 + var7) + var1;
 					var8.field1856[var16] = method2617(var14);
 					var8.field1823[var16] = -1;
-				} else {
+				}
+				else
+				{
 					var8.field1823[var16] = -2;
 				}
-			} else {
+			}
+			else
+			{
 //				if(def.field1741 != null && def.field1741[def.faceVertexIndices1[var16]] != null) {
 //					var13 = def.field1741[def.faceVertexIndices1[var16]];
 //				} else {
-					var13 = def.vertexNormals[def.faceVertexIndices1[var16]];
+				var13 = def.vertexNormals[def.faceVertexIndices1[var16]];
 				//}
 
 				var14 = (var4 * var13.y + var5 * var13.z + var3 * var13.x) / (var7 * var13.magnitude) + var1;
@@ -340,7 +406,7 @@ public class ItemSpriteFactory
 //				if(def.field1741 != null && def.field1741[def.faceVertexIndices2[var16]] != null) {
 //					var13 = def.field1741[def.faceVertexIndices2[var16]];
 //				} else {
-					var13 = def.vertexNormals[def.faceVertexIndices2[var16]];
+				var13 = def.vertexNormals[def.faceVertexIndices2[var16]];
 				//}
 
 				var14 = (var4 * var13.y + var5 * var13.z + var3 * var13.x) / (var7 * var13.magnitude) + var1;
@@ -348,8 +414,8 @@ public class ItemSpriteFactory
 //				if(def.field1741 != null && def.field1741[def.faceVertexIndices3[var16]] != null) {
 //					var13 = def.field1741[def.faceVertexIndices3[var16]];
 //				} else {
-					var13 = def.vertexNormals[def.faceVertexIndices3[var16]];
-			//	}
+				var13 = def.vertexNormals[def.faceVertexIndices3[var16]];
+				//	}
 
 				var14 = (var4 * var13.y + var5 * var13.z + var3 * var13.x) / (var7 * var13.magnitude) + var1;
 				var8.field1823[var16] = method2617(var14);
@@ -374,21 +440,29 @@ public class ItemSpriteFactory
 		return var8;
 	}
 
-	static final int method2608(int var0, int var1) {
+	static final int method2608(int var0, int var1)
+	{
 		var1 = (var0 & 127) * var1 >> 7;
-		if(var1 < 2) {
+		if (var1 < 2)
+		{
 			var1 = 2;
-		} else if(var1 > 126) {
+		}
+		else if (var1 > 126)
+		{
 			var1 = 126;
 		}
 
 		return (var0 & 65408) + var1;
 	}
 
-	static final int method2617(int var0) {
-		if(var0 < 2) {
+	static final int method2617(int var0)
+	{
+		if (var0 < 2)
+		{
 			var0 = 2;
-		} else if(var0 > 126) {
+		}
+		else if (var0 > 126)
+		{
 			var0 = 126;
 		}
 
