6256f3e3de4e875a575146610c894032718f14ad
SomeoneWithAnInternetConnection <34518321+SomeoneWithAnInternetConnection@users.noreply.github.com>
2018-03-13 00:49:41 -0400

    Kill some dead code

--- cache/src/main/java/net/runelite/cache/item/Renderable.java
+++ cache/src/main/java/net/runelite/cache/item/Renderable.java
@@ -12,25 +12,7 @@ public abstract class Renderable
 	}
 
 
-	protected Model getModel()
-	{
-		return null;
-	}
-
-
-	void draw(int var1, int var2, int var3, int var4, int var5, int var6, int var7, int var8, int var9)
-	{
-		Model var10 = this.getModel();
-		if (var10 != null)
-		{
-			this.modelHeight = var10.modelHeight;
-			var10.draw(var1, var2, var3, var4, var5, var6, var7, var8, var9);
-		}
-
-	}
-
-
-//   public static IndexedSprite getSprite(IndexDataBase var0, String var1, String var2) {
+	//   public static IndexedSprite getSprite(IndexDataBase var0, String var1, String var2) {
 //      int var3 = var0.getFile(var1);
 //      int var4 = var0.getChild(var3, var2);
 //      byte[] var7 = var0.getConfigData(var3, var4);
@@ -53,8 +35,4 @@ public abstract class Renderable
 //   }
 
 
-	public static boolean method3047(int var0)
-	{
-		return (var0 >> 31 & 1) != 0;
-	}
 }
